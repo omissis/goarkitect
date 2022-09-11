8ad3be4bc24e5bb6298b83d7012917d47db2eb93
omissis <claudi0.beatric3@gmail.com>
2022-09-11 16:06:08 +0200

    chore: remove scripts dir

--- scripts/foo.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-cd /root
-curl https://dl.google.com/go/go1.19.1.linux-arm64.tar.gz --output go.tgz
-tar xvfz go.tgz
-curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b $(go env GOPATH)/bin v1.49.0
-apt-get install -y git make
-export PATH=/root/go/bin:$PATH
